5cf881ffe0a1566d60798569322781a157ae5e89
Mr-Johny-Blaska <121683366+jan-blaska@users.noreply.github.com>
2023-07-25 13:27:54 +0200

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+import { posts } from "./posts.js";
 
+const oldagramPostsEl = document.getElementById("oldagram-posts");
+
+
+for (let i = 0; i < posts.length; i++) {
+    const post = `
+        <section class="section-1">
+            <div class="sub-container">
+                <div class="post-header">
+                <img class="avatar-icon" src=${posts[i].avatar} alt="post-creator-avatar">
+                <div class="post-header-text">
+                    <p><strong>${posts[i].name}</strong></p>
+                    <p>${posts[i].location}</p>
+                </div>
+            </div>
+            </div>
+            <img class="post-img" src=${posts[i].post} alt="post-image">
+        </section>
+
+        <section class="section-2">
+            <div class="sub-container">
+                <div class="icons-below-post">
+                    <img src="images/icon-heart.png" alt="likes-icon" onClick="addLikes()">
+                    <img src="images/icon-comment.png" alt="comments-icon">
+                    <img src="images/icon-dm.png" alt="send-message-icon">
+                </div>
+                <p><strong>${posts[i].likes} likes</strong></p>
+                <p><strong>${posts[i].username}</strong> ${posts[i].comment}</p>
+            </div>
+        </section>
+    `;
+    oldagramPostsEl.innerHTML += post;
+};
